f95165679d73bea89f57e9d7cba5f39692b88ff0
alexeykomov <alexeykcontact@gmail.com>
2017-10-06 11:51:07 +0300

    add: new aliases

--- aliases.sh
+++ aliases.sh
@@ -1,10 +1,22 @@
 PATH="/usr/local/bin:/usr/local/sbin:~/bin:$PATH:/usr/local/Cellar/sbt/0.13.13/bin:"
 export NVM_DIR=~/.nvm
 source ~/.nvm/nvm.sh
-nvm use v7.6.0 >> /dev/null
 
 #Aliases.
-alias edit="atom"
+alias edit="/Applications/TextMate.app/Contents/Resources/mate -w"
+
+alias up="cd .."
+alias findg="find . -type f -print0 | xargs -0 grep -Ini --color=auto"
+alias la="ls -a"
+
+PATH="/usr/local/bin:/usr/local/sbin:~/bin:$PATH:/usr/local/Cellar/sbt/0.13.13/bin:"
+export NVM_DIR=~/.nvm
+export EDITOR="/Applications/TextMate.app/Contents/Resources/mate -w"
+source ~/.nvm/nvm.sh
+nvm use v6.5.0 >> /dev/null
+
+#Aliases.
+alias edit="/Applications/TextMate.app/Contents/Resources/mate -w"
 
 alias up="cd .."
 alias findg="find . -type f -print0 | xargs -0 grep -Ini --color=auto"
@@ -17,7 +29,7 @@ alias unpack="tar -xzvf"
 # alias untarSimple="tar -xvf"
 
 # git protipz
-alias gg="git fetch && git status -sb"  # disable this if you find it too slow, and use the below one
+# alias gg="git fetch && git status -sb"  # disable this if you find it too slow, and use the below one
 alias gst="git status -sb"
 alias ga="git add"
 # alias gaa="git add -A"
@@ -46,4 +58,15 @@ alias ee="exit"
 alias qq="exit"
 
 # OneMW
-alias ssh_onemw="ssh -p 2222 onemwdev@127.0.0.1 -i ~/.ssh/onemw"
+alias ssh_onemw="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@box"
+alias scp_onemw="scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+alias tunnel_onemw="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -L 9229:localhost:9229 -N root@box"
+alias mount_onemw="ssh_onemw '/usr/sbin/iptables -F && mount -o nolock 10.6.3.145:/Users/alexk/Work/one-mw-shared/onemw-js/src /usr/share/lgioui'"
+
+##
+# Your previous /Users/alexk/.bash_profile file was backed up as /Users/alexk/.bash_profile.macports-saved_2017-02-28_at_19:43:23
+##
+
+# MacPorts Installer addition on 2017-02-28_at_19:43:23: adding an appropriate PATH variable for use with MacPorts.
+export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
+# Finished adapting your PATH environment variable for use with MacPorts.
